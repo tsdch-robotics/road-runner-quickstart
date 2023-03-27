ef67719fba785c8cfcad397fcb6427c8735a657b
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2023-03-27 08:41:07 -0400

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -33,7 +33,7 @@ public class DriveConstants {
      */
     public static final boolean RUN_USING_ENCODER = true;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(24, 0, 8,
-            2.3);
+            6.23);
 
     //getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
 
@@ -66,7 +66,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 42.3;
+    public static double MAX_VEL = 90;
     public static double MAX_ACCEL = 40;
     public static double MAX_ANG_VEL = 3.031419;
     public static double MAX_ANG_ACCEL = 1.047;
