53992493184b01fde2f4a7a5881bb00461fda8fe
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2023-07-13 14:58:33 -0400

    Create DanielTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DanielTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+/*
+ * This is an example of a more complex path to really test the tuning.
+ */
+@Autonomous(group = "drive")
+public class DanielTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Trajectory strafe = drive.trajectoryBuilder(new Pose2d())
+                .strafeLeft(36)
+                .build();
+        Trajectory forward = drive.trajectoryBuilder(strafe.end())
+                .forward(5)
+                .build();
+
+        drive.followTrajectory(strafe);
+        drive.followTrajectory(forward);
+
+    }
+}
